302a826d9ececdba4b92d5345ff86221438a4da6
LauraMenzel <menzel-laura@gmx.de>
2022-07-07 13:07:17 +0200

    exercise1

---
+++ solution.js
@@ -0,0 +1,10 @@
+const myArray = ["melon","apple","orange","kiwi","nuts"]
+console.log(myArray);
+
+
+const thirdElement = myArray[2];
+console.log(thirdElement);
+
+
+myArray[3]= "fourth element";
+console.log(myArray);
